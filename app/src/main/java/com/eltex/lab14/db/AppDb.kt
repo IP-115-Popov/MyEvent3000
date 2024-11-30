d2566e2c33cc4fba582dbb1e0cb8b2e653db7770
Sergey Popov <serzh.serp123.popov@mail.ru>
2024-11-30 16:40:47 +0700

    (Задание 21) Задача №3. Миграция бд на 2 версию

--- app/src/main/java/com/eltex/lab14/db/AppDb.kt
+++ app/src/main/java/com/eltex/lab14/db/AppDb.kt
@@ -4,12 +4,14 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.eltex.lab14.dao.EventDao
 import com.eltex.lab14.entity.EventEntity
 
 @Database(
     entities = [EventEntity::class],
-    version = 1,
+    version = 2,
 )
 abstract class AppDb : RoomDatabase() {
     abstract val eventDao: EventDao
@@ -28,11 +30,18 @@ abstract class AppDb : RoomDatabase() {
 
                 val appDb = Room.databaseBuilder(application, AppDb::class.java, "appdb")
                     .createFromAsset("database/appdb.db").fallbackToDestructiveMigration()
+                    .addMigrations(MIGRATION_1_2)
+                    //.fallbackToDestructiveMigration()
                     .allowMainThreadQueries().build()
 
                 INSTANCE = appDb
 
                 return appDb
+            }
+        }
+        val MIGRATION_1_2 = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("ALTER TABLE Events ADD COLUMN aboba INTEGER NOT NULL DEFAULT 0")
             }
         }
     }
